b7976f5e35171c9473ef661bf1d6e0adaf91ab28
beliemun <beliemun@paran.com>
2024-07-25 00:10:38 +0900

    Updated Pagination

--- src/app/(with-menu)/(2.foundation)/pagination/page.tsx
+++ src/app/(with-menu)/(2.foundation)/pagination/page.tsx
@@ -1,11 +1,25 @@
-import { Divider, Section } from "@/components/atoms";
+import { Divider, Pagination, Section } from "@/components/atoms";
 import { PageLayout } from "@/components/organisms";
 
 export default function () {
   return (
     <PageLayout title="<Pagination />">
       <Divider orientation="left">Basic Pagination</Divider>
-      <Section className="flex flex-col flex-wrap gap-4 pt-2"></Section>
+      <Section className="flex flex-col flex-wrap gap-4 pt-2">
+        <Pagination defaultCurrent={1} total={50} />
+      </Section>
+      <Divider orientation="left">More Pages</Divider>
+      <Section className="flex flex-col flex-wrap gap-4 pt-2">
+        <Pagination defaultCurrent={6} total={500} />
+      </Section>
+      <Divider orientation="left">QuickJumper</Divider>
+      <Section className="flex flex-col flex-wrap gap-4 pt-2">
+        <Pagination defaultCurrent={6} total={500} showQuickJumper />
+      </Section>
+      <Divider orientation="left">Disabled</Divider>
+      <Section className="flex flex-col flex-wrap gap-4 pt-2">
+        <Pagination defaultCurrent={6} total={500} disabled />
+      </Section>
     </PageLayout>
   );
 }
--- src/app/(with-menu)/(2.foundation)/radio/page.tsx
+++ src/app/(with-menu)/(2.foundation)/radio/page.tsx
@@ -1,26 +1,42 @@
 "use client";
 
-import { Radio, Section, Text } from "@/components/atoms";
+import { Card, Radio, Section, Space, Text } from "@/components/atoms";
 import { PageLayout } from "@/components/organisms";
+import { Divider } from "antd";
 
 export default function () {
   return (
     <PageLayout title="<Radio />">
       <Section className="flex flex-row flex-wrap gap-4 px-8">
-        <Radio.Group className="space-x-4" defaultValue={2}>
-          <Radio value={1}>
-            <Text type="sm-normal">Option A</Text>
-          </Radio>
-          <Radio value={2}>
-            <Text type="sm-normal">Option B</Text>
-          </Radio>
-          <Radio value={3}>
-            <Text type="sm-normal">Option C</Text>
-          </Radio>
-          <Radio value={4} disabled>
-            <Text type="sm-normal">Option D</Text>
-          </Radio>
-        </Radio.Group>
+        <Card title={"Variants"}>
+          <Space direction="horizontal" gap={16}>
+            <Radio value={1}>
+              <Text type="sm-normal">Default</Text>
+            </Radio>
+            <Radio value={2} checked>
+              <Text type="sm-normal">Checked</Text>
+            </Radio>
+            <Radio value={3} disabled>
+              <Text type="sm-normal">Disabled</Text>
+            </Radio>
+          </Space>
+        </Card>
+        <Card title={"Radio Group"}>
+          <Radio.Group className="space-x-4" defaultValue={2}>
+            <Radio value={1}>
+              <Text type="sm-normal">Option A</Text>
+            </Radio>
+            <Radio value={2}>
+              <Text type="sm-normal">Option B</Text>
+            </Radio>
+            <Radio value={3}>
+              <Text type="sm-normal">Option C</Text>
+            </Radio>
+            <Radio value={4}>
+              <Text type="sm-normal">Option D</Text>
+            </Radio>
+          </Radio.Group>
+        </Card>
       </Section>
     </PageLayout>
   );
