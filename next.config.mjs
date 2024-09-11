fc6cbc2d6e4c918f7b04804933b8097477cb0d01
beliemun <beliemun@paran.com>
2024-09-11 11:25:41 +0900

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -16,15 +16,6 @@ const nextConfig = {
     compiler: {
         styledComponents: true
     },
-    // webpack: (config) => {
-    //     config.externals.push({
-    //         "utf-8-validate": "commonjs utf-8-validate",
-    //         bufferutil: "commonjs bufferutil",
-    //         canvas: "commonjs canvas",
-    //     });
-    //     // config.infrastructureLogging = { debug: /PackFileCache/ };
-    //     return config;
-    // },
 };
 
 export default nextConfig;
