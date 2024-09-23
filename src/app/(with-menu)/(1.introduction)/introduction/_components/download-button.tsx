95f58e70e6e21e6e900460481d9fba3e6d4a7c10
beliemun <beliemun@paran.com>
2024-09-23 15:29:16 +0900

    Update download-button.tsx

--- src/app/(with-menu)/(1.introduction)/introduction/_components/download-button.tsx
+++ src/app/(with-menu)/(1.introduction)/introduction/_components/download-button.tsx
@@ -1,19 +1,35 @@
 "use client";
 import { Animate, Button } from "@/components/atoms";
 
-const URL = "https://drive.google.com/uc?export=download&id=15uBefeQLhTjXKov9s-ed9IJUgDDkqXIQ";
+const RESUME_PDF = "https://bit.ly/4dxBNdh";
+const CARRER_PDF = "https://bit.ly/47ORw6k";
+const CARRER_WORD = "https://bit.ly/3BtzHh6";
 
 export const DownloadButton = () => {
-  const handleClick = () => {
-    window.location.href = URL;
+  const handleClick = (url: string) => {
+    window.location.href = url;
   };
 
   return (
-    <div className="fixed bottom-0 right-0 p-8">
+    <div className="fixed bottom-0 right-0 p-2 sm:p-8">
       <Animate>
-        <Button onClick={handleClick} className="shadow-md">
-          이력서 PDF 다운로드
-        </Button>
+        <div className="flex flex-col items-end gap-2 sm:gap-4">
+          <div>
+            <Button onClick={() => handleClick(RESUME_PDF)} className="block shadow-md">
+              이력서(PDF) 다운로드
+            </Button>
+          </div>
+          <div>
+            <Button onClick={() => handleClick(CARRER_PDF)} className="shadow-md">
+              경력기술서(PDF) 다운로드
+            </Button>
+          </div>
+          <div>
+            <Button onClick={() => handleClick(CARRER_WORD)} className="shadow-md">
+              경력기술서(WORD) 다운로드
+            </Button>
+          </div>
+        </div>
       </Animate>
     </div>
   );
